a64b63a69d9f07bb7ee0d958bd6ba833bf9a2711
Adrian Moennich <adrian.moennich@cern.ch>
2018-04-03 14:25:42 +0200

    Restructure to separate browser and dev/cli code
    - different babel configs
    - cli stuff should never be imported when iporting browser code

--- src/dev/cli/compile.js
+++ src/dev/cli/compile.js
@@ -2,4 +2,5 @@
 
 import poToReact from '../po2react';
 
+
 process.stdout.write(JSON.stringify(poToReact(process.argv[2])) + '\n');
--- src/dev/index.js
+++ src/dev/index.js
@@ -1,3 +1,2 @@
-export {makeComponents, Singular, Plural, Param} from './components';
 export {default as makeI18nPlugin} from './extract-plugin';
 export {default as poToReact} from './po2react';

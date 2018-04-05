da06b23a4fce2fc8ab9e4f8f85969f9323c39b6b
Adrian Moennich <adrian.moennich@cern.ch>
2018-04-05 10:41:02 +0200

    Run eslint for tests as well

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,17 +1,20 @@
+/* global test:false, expect:false */
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import {makeComponents, Param, Singular, Plural} from '../src/client';
 
+
 const expectJSX = (jsx) => expect(renderer.create(jsx).toJSON());
 const gettext = expression => expression.split('').reverse().join('');
-const ngettext = (sExpression, pExpression, n) => n > 1 ? pExpression.toUpperCase() : sExpression.toLowerCase();
+const ngettext = (sExpression, pExpression, n) => (n > 1 ? pExpression.toUpperCase() : sExpression.toLowerCase());
 
 const {Translate, PluralTranslate} = makeComponents(gettext, ngettext);
 
 test('Basic translation works', () => {
     expectJSX(<Translate>Fetchez la vache</Translate>).toBe('ehcav al zehcteF');
-    expectJSX(<Translate>Fetchez la vache <Param name="number" value={5} /></Translate>).toBe('}rebmun{ ehcav al zehcteF')
+    expectJSX(<Translate>Fetchez la vache <Param name="number" value={5} /></Translate>).toBe('}rebmun{ ehcav al zehcteF');
 });
 
 test('Pluralized translation works', () => {
@@ -27,4 +30,4 @@ test('Pluralized translation works', () => {
     );
     expectJSX(message(1)).toBe('this is a vache');
     expectJSX(message(2)).toBe('THIS IS {COUNT} VACHES');
-})
+});
